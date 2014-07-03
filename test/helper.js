8e660092dcb7ae99f1dafb8d2053d82670c6001a
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-02 23:21:50 -0400

    Created test helper to clear database

---
+++ test/helper.js
@@ -0,0 +1,24 @@
+exports = module.exports = {
+  clearUsers: function() {
+    global.DB.users = {};
+  },
+
+  clearFollows: function() {
+    global.DB.follows = {};
+  },
+
+  clearListens: function() {
+    global.DB.listens = {};
+  },
+
+  clearMusic: function() {
+    global.DB.music = {};
+  },
+
+  clearAll: function() {
+    exports.clearUsers();
+    exports.clearFollows();
+    exports.clearListens();
+    exports.clearMusic();
+  }
+};
