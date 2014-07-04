7b94c1c71fd7bd109cc16813d205e8122e69cb40
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-03 23:59:52 -0400

    Refactored users endpoint tests

--- test/resources/users.js
+++ test/resources/users.js
@@ -3,68 +3,101 @@ var request = require('supertest');
 var api = require('../..');
 var helper = require('../helper');
 
-describe('GET /users', function(){
-  it('should respond with users', function(done){
-    var app = api();
+describe('/users', function(){
+  describe('POST', function() {
+    describe('with no id', function() {
+      it('returns 400', function(done) {
+        var app = api();
 
-    request(app.listen())
-    .get('/users')
-    .expect(200)
-    .end(function(err, res){
-      if (err) return done(err);
-      res.body.should.eql({});
-      done();
+        request(app.listen())
+        .post('/users')
+        .send({})
+        .expect(400)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+
+    describe('with undefined id', function() {
+      it('returns 400', function(done) {
+        var app = api();
+
+        request(app.listen())
+        .post('/users')
+        .send({id: ''})
+        .expect(400)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+
+    describe('with id', function() {
+      it('returns 201', function(done) {
+        var app = api();
+
+        request(app.listen())
+        .post('/users')
+        .expect(201)
+        .send({id: 'a'})
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
+
+      describe('with a duplicated id', function() {
+        it('returns 400', function(done) {
+          var app = api();
+
+          request(app.listen())
+          .post('/users/')
+          .send({id: 'a'})
+          .expect(400)
+          .end(function(err, res){
+            if (err) return done(err);
+            done();
+          });
+        });
+      });
     });
   });
 
-  it('should respond with users/:id', function(done){
-    var app = api();
+  describe('GET', function() {
 
-    request(app.listen())
-    .post('/users/')
-    .send({id: 'a'})
-    .expect(201)
-    .end(function(err, res){
-      if (err) return done(err);
-      done();
+    describe('with saved user', function() {
+      it('returns 200', function(done){
+        var app = api();
+
+        request(app.listen())
+        .get('/users')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
     });
-  });
 
-  it('should respond with bad request (400) and id required', function(done) {
-    var app = api();
+    describe('with no saved user', function() {
+      before(function() {
+        helper.clearUsers();
+      });
 
-    request(app.listen())
-    .post('/users/')
-    .send({})
-    .expect(400)
-    .end(function(err, res){
-      if (err) return done(err);
-      done();
-    });
-  });
+      it('returns 200', function(done) {
+        var app = api();
 
-  it('should respond with bad request (400) and id exists', function(done) {
-    var app = api();
-
-    request(app.listen())
-    .post('/users/')
-    .send({id: 'a'})
-    .expect(400)
-    .end(function(err, res){
-      if (err) return done(err);
-      done();
-    });
-  });
-
-  it('should respond with saved users', function(done){
-    var app = api();
-
-    request(app.listen())
-    .get('/users')
-    .expect(200)
-    .end(function(err, res){
-      if (err) return done(err);
-      done();
+        request(app.listen())
+        .get('/users')
+        .expect(200)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
     });
   });
 
