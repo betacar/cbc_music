e783858fb5426f79bf8c1b50d64465882f511e71
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-05 15:05:38 -0400

    Music model: - All and exists class methods created. - Save instance method to save music tracks. - Tests for the model.

---
+++ app/music/model.js
@@ -0,0 +1,37 @@
+/**
+ * Music model.
+ */
+
+function Music (id, generes) {
+  if (!id && !generes) throw new Error('An id and a array of generes are required');
+  if (!id) throw new Error('An id must be provided');
+  if (!generes) throw new Error('Generes must be provided. ie. "folk,funk"]');
+  this.id      = id;
+  this.generes = generes;
+
+  return this;
+};
+
+/**
+ * Class methods.
+ */
+
+Music.all = function() {
+  return DB.music;
+};
+
+Music.exists = function(id) {
+  return (id in DB.music);
+};
+
+/**
+ * Instance methods.
+ */
+
+Music.prototype.save = function() {
+  if (Music.exists(this.id)) throw new Error('music id ' + this.id + ' already exists in the database');
+  DB.music[this.id] = this.generes;
+  return DB.music[this.id];
+};
+
+module.exports = Music;
--- test/models/music.js
+++ test/models/music.js
@@ -14,6 +14,62 @@ describe('The music model', function() {
     });
   });
 
+  it('saves a music track without error', function(){
+    var music = new Music('m1', 'jazz,ska');
+    music.save();
+  });
+
+  it('returns an exception if the id exists', function(){
+    (function() {
+      var music = new Music('m1', 'jazz,ska');
+      music.save();
+    }).should.throw(Error('music id a already exists in the database'));
+  });
+
+  it('returns the saved music id and array of generes', function() {
+    var music = new Music('m2', 'floklore,joropo');
+    music.save();
+    music.id.should.eql('m2');
+    music.generes.should.eql('floklore,joropo');
+  });
+
+  it('returns an exception when no parameters is passed', function() {
+    (function() {
+      new Music();
+    }).should.throw(Error('An id and a array of generes are required'));
+  });
+
+  it('returns an exception when no ID is passed', function() {
+    (function() {
+      new Music(undefined, 'rock,metal');
+    }).should.throw(Error('An id string must be provided'));
+  });
+
+  it('returns an exception when no generes are passed', function() {
+    (function() {
+      new Music('m5');
+    }).should.throw(Error('An array of generes must be provided. ie. ["folk", "funk"]'));
+  });
+
+  it('returns true if a music exists', function() {
+    var music = new Music('m3', 'electronic,house');
+    music.save();
+    Music.exists('m3').should.be.true;
+  });
+
+  it('returns false if a music does not exists', function() {
+    Music.exists('m9').should.be.false;
+  });
+
+  it('lists all music tracks', function() {
+    expected = {
+      m1: 'jazz,ska',
+      m2: 'floklore,joropo',
+      m3: 'electronic,house'
+    };
+    Music.all().should.eql(expected);
+  });
+
   after(function() {
     helper.clearMusic();
   });
