139c829381de8880472d84033f595c9dcac6715f
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-05 14:10:18 -0400

    Basic music model tests

--- test/models/music.js
+++ test/models/music.js
@@ -4,6 +4,16 @@ var helper  = require('../helper');
 var Music   = require('../../app/music/model');
 
 describe('The music model', function() {
+  describe('with no saved music', function() {
+    before(function() {
+      helper.clearMusic();
+    });
+
+    it('returns an empty object', function() {
+      Music.all().should.eql({});
+    });
+  });
+
   after(function() {
     helper.clearMusic();
   });
