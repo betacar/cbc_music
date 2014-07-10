82c3517701870cdb5c1a9c0e5e8e7441cae7c311
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-09 22:45:29 -0400

    Removed unneeded music model checkpoint in music resource: is already done in the model

--- app/music/index.js
+++ app/music/index.js
@@ -14,7 +14,6 @@ exports.create = function *(id, generes){
   var body = yield parse(this);
   if (!body.id) this.throw(400, '.id is required');
   if (!body.generes) this.throw(400, '.generes are required');
-  if (Music.exists(body.id)) this.throw(400, id + ' exists');
 
   try {
     music = new Music(body.id, body.generes);
