f9260cc6a79317b713ae1e513370205e0451f0e4
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-20 22:13:13 -0400

    Recommendation resource tests added

---
+++ test/resources/recommendations.js
@@ -0,0 +1,91 @@
+
+var request = require('supertest');
+var api = require('../..');
+var helper = require('../helper');
+
+describe('GET /recommendations', function() {
+  after(function () {
+    return helper.clearAll();
+  });
+
+  describe('with no saved data', function() {
+    before(function() {
+      return helper.clearAll();
+    });
+
+    it('returns 400', function(done){
+      var app = api();
+
+      request(app.listen())
+      .get('/recommendations/a')
+      .expect(400)
+      .end(function(err, res){
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    describe('with a user param undefined', function() {
+      it('returns 404', function(done) {
+        var app = api();
+
+        request(app.listen())
+        .get('/recommendations/')
+        .expect(404)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('with saved data', function() {
+    before(function() {
+      return helper.loadAll();
+    });
+
+    describe('with a user param undefined', function() {
+      it('returns 404', function(done) {
+        var app = api();
+
+        request(app.listen())
+        .get('/recommendations/')
+        .expect(404)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+
+    describe('with a unexisting user', function() {
+      it('returns 400', function(done) {
+        var app = api();
+
+        request(app.listen())
+        .get('/recommendations/betacar')
+        .expect(400)
+        .end(function(err, res){
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+
+    it('returns 200', function(done) {
+      var app = api();
+
+      request(app.listen())
+      .get('/recommendations/a')
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        res.body.should.be.ok;
+        res.body.list.should.be.an.Array;
+        res.body.list.should.have.length(5);
+        done();
+      });
+    });
+  });
+});
