59536c9e8753dbeb288651648cf7e9ccb154fa3f
Carlos Betancourt Carrero <cbetancourtcarr@groupon.com>
2014-07-20 21:45:44 -0400

    Added loaders to the test helper

--- test/helper.js
+++ test/helper.js
@@ -1,4 +1,10 @@
 global.DB = global.DB || { users: {}, follows: [], music: {}, listen: [] };
+var async = require('async');
+var stubs = {
+  users: require('./stub/users.json'),
+  follows: require('./stub/follows.json'),
+  listens: require('./stub/listen.json'),
+};
 
 exports = module.exports = {
   clearUsers: function() {
@@ -22,5 +28,53 @@ exports = module.exports = {
     exports.clearFollows();
     exports.clearMusic();
     exports.clearListens();
+  },
+
+  loadUsers: function() {
+    var count = 1;
+
+    async.each(stubs.users, function(user, cb) {
+      global.DB.users[user.id] = {};
+      count++;
+
+      if (stubs.users.length === count) return cb();
+    }, function(err) {
+      if (err) throw new Error(err);
+      return true;
+    });
+  },
+
+  loadFollows: function() {
+    var count = 1;
+
+    async.each(stubs.follows, function(follow, cb) {
+      global.DB.follows.push({ from: follow.from, to: follow.to });
+      count++;
+
+      if (stubs.follows.length === count) return cb();
+    }, function(err) {
+      if (err) throw new Error(err);
+      return true;
+    });
+  },
+
+  loadListen: function() {
+    var count = 1;
+
+    async.each(stubs.listens, function(listen, cb) {
+      global.DB.listen.push({user: listen.user, music: listen.music});
+      count++;
+
+      if (stubs.listens.length === count) return cb();
+    }, function(err) {
+      if (err) throw new Error(err);
+      return true;
+    });
+  },
+
+  loadAll: function() {
+    exports.loadUsers();
+    exports.loadFollows();
+    exports.loadListen();
   }
 };
